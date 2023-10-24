edb96e6e6ea70086898e64c45c3cbd729b3afd04
IKSEONG.AHN <ikseong.ahn@gi-vita.io>
2023-10-24 20:36:28 +0900

    feat : [RE-000] example

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,23 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("SecretNumber {} ", secret_number);
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess: u32 = guess.trim().parse().expect("Error parse");
+    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Less"),
+        Ordering::Greater => println!("Greater"),
+        Ordering::Equal => println!("Equal"),
+    }
+
+    println!("you guessed {}", guess)
+}
